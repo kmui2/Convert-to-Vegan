11e51e7c340c6560c5e878fd6411d9115da17c8d
Nathan Somers <nsomers2@illinois.edu>
2017-11-11 15:42:02 -0600

    added some comments, did not change the functionality

--- content.js
+++ content.js
@@ -1,13 +1,13 @@
-var elements = document.getElementsByTagName('*');
+var elements = document.getElementsByTagName('*'); //create an array of all the tag names, hence the *
 
 for (var element of elements) {
 
-    for (var node of element.childNodes) { 
+    for (var node of element.childNodes) { //childnodes of each element are a list detailing the tags
 
         // get text part of the element
-        if (node.nodeType === 3) {
+        if (node.nodeType === 3) { //node type 3 is text
             var text = node.nodeValue;
-            var replacedText = text.replace(/Trump/gi, 'Beef');
+            var replacedText = text.replace('Trump', 'Beef');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
