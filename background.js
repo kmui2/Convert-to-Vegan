a101344950d7d5c5ab70c428e0c7a75c5568a7ff
ffanizza <ffanizza@hawk.iit.edu>
2017-11-11 22:52:00 -0600

    merge

--- background.js
+++ background.js
@@ -4,10 +4,54 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 	toggle = !toggle;
 	if(toggle){
 		chrome.browserAction.setIcon({path: "on.png", tabId:tab.id}); //on
-		chrome.tabs.executeScript(tab.id, {file:"content.js"});
+		chrome.tabs.executeScript(tab.id, replaceWords);
 	}
 	else{
 		chrome.browserAction.setIcon({path: "off.png", tabId:tab.id}); //off
 		//chrome.tabs.executeScript(tab.id, {code:"alert()"});
 	}
 });
+
+var replaceWords = function () {
+	
+		var elements = document.getElementsByTagName('*');
+	
+		// // var keyString = 'a string',
+		// //     keyObj = {},
+		// //     keyFunc = function() {};
+		//
+		// setting the values
+		var veganMap = {
+			"egg": "tofu",
+			"milk": "almondmi1k"
+		}
+		// // getting the values
+		// myMap.get(keyString);    // "value associated with 'a string'"
+		// myMap.get(keyObj);       // "value associated with keyObj"
+		// myMap.get(keyFunc);      // "value associated with keyFunc"
+		//
+		// myMap.get('a string');   // "value associated with 'a string'"
+		//                          // because keyString === 'a string'
+		// myMap.get({});           // undefined, because keyObj !== {}
+		// myMap.get(function() {}) // undefined, because ke
+	
+		for (var element of elements) {
+			for (var node of element.childNodes) {
+				// get text part of the element
+				if (node.nodeType === 3) {
+					var textToCheck = node.nodeValue;
+					var listToCheck = textToCheck.split(" ");
+					for (var listItem in listToCheck) {
+						if (veganMap.hasOwnProperty(listToCheck[listItem])) { // is listItem == some key in map
+							var veganOption = veganMap.listItem; // get value from map
+							var newRegex = new RegExp(listToCheck[listItem], "gi");
+							var replacedText = listToCheck[listItem].replace(newRegex, veganMap[listToCheck[listItem]]);
+							if (replacedText !== listToCheck[listItem]) {
+								element.replaceChild(document.createTextNode(replacedText), node.nodeValue);
+							}
+						}
+					}
+				}
+			}
+		}
+	};
--- content.js
+++ content.js
@@ -4,39 +4,22 @@ script.addEventListener('load', function () {
 
     var elements = document.getElementsByTagName('*');
 
-    // // var keyString = 'a string',
-    // //     keyObj = {},
-    // //     keyFunc = function() {};
-    //
-    // setting the values
     var veganMap = {
         "egg": "tofu",
-        "milk": "almondmilk"
+        "milk": "almondmlk"
     }
-    // // getting the values
-    // myMap.get(keyString);    // "value associated with 'a string'"
-    // myMap.get(keyObj);       // "value associated with keyObj"
-    // myMap.get(keyFunc);      // "value associated with keyFunc"
-    //
-    // myMap.get('a string');   // "value associated with 'a string'"
-    //                          // because keyString === 'a string'
-    // myMap.get({});           // undefined, because keyObj !== {}
-    // myMap.get(function() {}) // undefined, because ke
-
+    
     for (var element of elements) {
         for (var node of element.childNodes) {
             // get text part of the element
             if (node.nodeType === 3) {
-                var textToCheck = node.nodeValue;
-                var listToCheck = textToCheck.split(" ");
-                for (var listItem in listToCheck) {
-                    if (veganMap.hasOwnProperty(listToCheck[listItem])) { // is listItem == some key in map
-                        var veganOption = veganMap.listItem; // get value from map
-                        var newRegex = new RegExp(listToCheck[listItem], "gi");
-                        var replacedText = listToCheck[listItem].replace(newRegex, veganMap[listToCheck[listItem]]);
-                        if (replacedText !== listToCheck[listItem]) {
-                            element.replaceChild(document.createTextNode(replacedText), node);
-                        }
+                for (var listItem in veganMap) {
+                    var text = node.nodeValue;
+                    var veganOption = veganMap[listItem]; // get value from map
+                    var newRegex = new RegExp(listItem,"gi");
+                    var replacedText = text.replace(newRegex, veganOption);
+                    if (replacedText !== text) {
+                        element.replaceChild(document.createTextNode(replacedText), node);
                     }
                 }
             }
