8d51f6308de869eb5dc6de01271ee0038aad1b08
kmui2 <kmui2@wisc.edu>
2017-11-11 18:44:19 -0600

    added firebase

--- content.js
+++ content.js
@@ -1,65 +1,83 @@
-var elements = document.getElementsByTagName('*');
+var script = document.createElement('script');
+script.src = "https://www.gstatic.com/firebasejs/4.6.2/firebase.js";
+script.addEventListener('load', function() {
+  // SomeObject is available!!!
+  // Initialize Firebase
+  var config = {
+  apiKey: "AIzaSyAL8RlZzUfXn18VzMCCooXE7Q58lnRpYVM",
+  authDomain: "convert-to-vegan.firebaseapp.com",
+  databaseURL: "https://convert-to-vegan.firebaseio.com",
+  projectId: "convert-to-vegan",
+  storageBucket: "convert-to-vegan.appspot.com",
+  messagingSenderId: "200949757458"
+  };
+  firebase.initializeApp(config);
+  
+  var elements = document.getElementsByTagName('*');
+  
+  // // var keyString = 'a string',
+  // //     keyObj = {},
+  // //     keyFunc = function() {};
+  //
+  // setting the values
+  var veganMap = {
+      "egg": "tofu",
+      "Egg": "Tofu",
+      "milk": "almondmilk",
+      "Milk": "Almondmilk"
+  }
+  // // getting the values
+  // myMap.get(keyString);    // "value associated with 'a string'"
+  // myMap.get(keyObj);       // "value associated with keyObj"
+  // myMap.get(keyFunc);      // "value associated with keyFunc"
+  //
+  // myMap.get('a string');   // "value associated with 'a string'"
+  //                          // because keyString === 'a string'
+  // myMap.get({});           // undefined, because keyObj !== {}
+  // myMap.get(function() {}) // undefined, because ke
+  
+  for (var element of elements) {
+  
+      for (var node of element.childNodes) {
+  
+          // get text part of the element
+          if (node.nodeType === 3) {
+  
+              var nonVegan = node.nodeValue;
+  
+              if (veganMap.hasOwnProperty(nonVegan)) {
+  
+                  var veganOption = veganMap.nonVegan;
+                  var newRegex = new RegExp(nonVegan,"gi");
+  
+                  var replacedText = nonVegan.replace(newRegex, veganMap.nonVegan);
+  
+                  if (replacedText !== nonVegan) {
+                      element.replaceChild(document.createTextNode(replacedText), node);
+                  }
+  
+              }
+          }
+      }
+  }
+  //
+  // THIS WORKS:
+  // for (var element of elements) {
+  //
+  //     for (var node of element.childNodes) {
+  //
+  //         // get text part of the element
+  //         if (node.nodeType === 3) {
+  //             var text = node.nodeValue;
+  //             var newRegex = new RegExp("Trump","gi");
+  //             var replacedText = text.replace(newRegex, 'Beef');
+  //
+  //             if (replacedText !== text) {
+  //                 element.replaceChild(document.createTextNode(replacedText), node);
+  //             }
+  //         }
+  //     }
+  // }
+});
+document.head.appendChild(script);
 
-// // var keyString = 'a string',
-// //     keyObj = {},
-// //     keyFunc = function() {};
-//
-// setting the values
-var veganMap = {
-    "egg": "tofu",
-    "Egg": "Tofu",
-    "milk": "almondmilk",
-    "Milk": "Almondmilk"
-}
-// // getting the values
-// myMap.get(keyString);    // "value associated with 'a string'"
-// myMap.get(keyObj);       // "value associated with keyObj"
-// myMap.get(keyFunc);      // "value associated with keyFunc"
-//
-// myMap.get('a string');   // "value associated with 'a string'"
-//                          // because keyString === 'a string'
-// myMap.get({});           // undefined, because keyObj !== {}
-// myMap.get(function() {}) // undefined, because ke
-
-for (var element of elements) {
-
-    for (var node of element.childNodes) {
-
-        // get text part of the element
-        if (node.nodeType === 3) {
-
-            var nonVegan = node.nodeValue;
-
-            if (veganMap.hasOwnProperty(nonVegan)) {
-
-                var veganOption = veganMap.nonVegan;
-                var newRegex = new RegExp(nonVegan,"gi");
-
-                var replacedText = nonVegan.replace(newRegex, veganMap.nonVegan);
-
-                if (replacedText !== nonVegan) {
-                    element.replaceChild(document.createTextNode(replacedText), node);
-                }
-
-            }
-        }
-    }
-}
-//
-// THIS WORKS:
-// for (var element of elements) {
-//
-//     for (var node of element.childNodes) {
-//
-//         // get text part of the element
-//         if (node.nodeType === 3) {
-//             var text = node.nodeValue;
-//             var newRegex = new RegExp("Trump","gi");
-//             var replacedText = text.replace(newRegex, 'Beef');
-//
-//             if (replacedText !== text) {
-//                 element.replaceChild(document.createTextNode(replacedText), node);
-//             }
-//         }
-//     }
-// }
