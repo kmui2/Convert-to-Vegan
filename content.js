ca3a6fe5604376c0e5d4228e75c0d1649c067aae
ffanizza <ffanizza@hawk.iit.edu>
2017-11-11 18:17:31 -0600

    changed trump->beef to egg->tofu, milk->almondmilk. using js object

--- content.js
+++ content.js
@@ -1,17 +1,65 @@
 var elements = document.getElementsByTagName('*');
 
+// // var keyString = 'a string',
+// //     keyObj = {},
+// //     keyFunc = function() {};
+//
+// setting the values
+var veganMap = {
+    "egg": "tofu",
+    "Egg": "Tofu",
+    "milk": "almondmilk",
+    "Milk": "Almondmilk"
+}
+// // getting the values
+// myMap.get(keyString);    // "value associated with 'a string'"
+// myMap.get(keyObj);       // "value associated with keyObj"
+// myMap.get(keyFunc);      // "value associated with keyFunc"
+//
+// myMap.get('a string');   // "value associated with 'a string'"
+//                          // because keyString === 'a string'
+// myMap.get({});           // undefined, because keyObj !== {}
+// myMap.get(function() {}) // undefined, because ke
+
 for (var element of elements) {
 
-    for (var node of element.childNodes) { 
+    for (var node of element.childNodes) {
 
         // get text part of the element
         if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/Trump/gi, 'Beef');
 
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
+            var nonVegan = node.nodeValue;
+
+            if (veganMap.hasOwnProperty(nonVegan)) {
+
+                var veganOption = veganMap.nonVegan;
+                var newRegex = new RegExp(nonVegan,"gi");
+
+                var replacedText = nonVegan.replace(newRegex, veganMap.nonVegan);
+
+                if (replacedText !== nonVegan) {
+                    element.replaceChild(document.createTextNode(replacedText), node);
+                }
+
             }
         }
     }
 }
+//
+// THIS WORKS:
+// for (var element of elements) {
+//
+//     for (var node of element.childNodes) {
+//
+//         // get text part of the element
+//         if (node.nodeType === 3) {
+//             var text = node.nodeValue;
+//             var newRegex = new RegExp("Trump","gi");
+//             var replacedText = text.replace(newRegex, 'Beef');
+//
+//             if (replacedText !== text) {
+//                 element.replaceChild(document.createTextNode(replacedText), node);
+//             }
+//         }
+//     }
+// }
