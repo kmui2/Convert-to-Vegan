22813999c00527af239788db70ececafe7b24cb7
kmui2 <kmui2@wisc.edu>
2017-11-11 22:19:50 -0600

    implemented toggle

--- content.js
+++ content.js
@@ -1,46 +1,74 @@
 var script = document.createElement('script');
-script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js";
-script.addEventListener('load', function () {
+var toggle = false;
+var prevToggle = false; // prevents duplicate messages
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        toggle = request.toggle;
+        if (toggle != prevToggle) {
+            if (toggle) {
+                convertToVegan();
+            }
+            else {
+                convertBack();
+            }
+        }
+        prevToggle = toggle;
+        console.log(toggle);
+        sendResponse({ toggle: toggle });
+    }
+);
 
+function convertToVegan() {
     var elements = document.getElementsByTagName('*');
 
-    // // var keyString = 'a string',
-    // //     keyObj = {},
-    // //     keyFunc = function() {};
-    //
     // setting the values
     var veganMap = {
         "egg": "tofu",
         "milk": "almondmilk"
     }
-    // // getting the values
-    // myMap.get(keyString);    // "value associated with 'a string'"
-    // myMap.get(keyObj);       // "value associated with keyObj"
-    // myMap.get(keyFunc);      // "value associated with keyFunc"
-    //
-    // myMap.get('a string');   // "value associated with 'a string'"
-    //                          // because keyString === 'a string'
-    // myMap.get({});           // undefined, because keyObj !== {}
-    // myMap.get(function() {}) // undefined, because ke
+    var elements = document.getElementsByTagName('*');
+    for (var nonVegan in veganMap) {
+        for (var element of elements) {
+            for (var node of element.childNodes) {
+                // get text part of the element
+                if (node.nodeType === 3) {
+                    var text = node.nodeValue;
+                    var newRegex = new RegExp('[\w-]*' + nonVegan + '[\w-]*', "gi");
+                    var replacedText = text.replace(newRegex, veganMap[nonVegan]);
 
-    for (var element of elements) {
-        for (var node of element.childNodes) {
-            // get text part of the element
-            if (node.nodeType === 3) {
-                var textToCheck = node.nodeValue;
-                var listToCheck = textToCheck.split(" ");
-                for (var listItem in listToCheck) {
-                    if (veganMap.hasOwnProperty(listToCheck[listItem])) { // is listItem == some key in map
-                        var veganOption = veganMap.listItem; // get value from map
-                        var newRegex = new RegExp(listToCheck[listItem], "gi");
-                        var replacedText = listToCheck[listItem].replace(newRegex, veganMap[listToCheck[listItem]]);
-                        if (replacedText !== listToCheck[listItem]) {
-                            element.replaceChild(document.createTextNode(replacedText), node);
-                        }
+                    if (replacedText !== text) {
+                        element.replaceChild(document.createTextNode(replacedText), node);
                     }
                 }
             }
         }
     }
-});
-document.head.appendChild(script);
+}
+
+function convertBack() {
+    var elements = document.getElementsByTagName('*');
+
+    // setting the values
+    var veganMap = {
+        "egg": "tofu",
+        "milk": "almondmilk"
+    }
+    var elements = document.getElementsByTagName('*');
+    for (var nonVegan in veganMap) {
+        for (var element of elements) {
+            for (var node of element.childNodes) {
+                // get text part of the element
+                if (node.nodeType === 3) {
+                    var text = node.nodeValue;
+                    var newRegex = new RegExp('[\w-]*' + veganMap[nonVegan] + '[\w-]*', "gi");
+                    var replacedText = text.replace(newRegex, nonVegan);
+
+                    if (replacedText !== text) {
+                        element.replaceChild(document.createTextNode(replacedText), node);
+                    }
+                }
+            }
+        }
+
+    }
+}
