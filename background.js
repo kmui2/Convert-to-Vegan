b99fd20f3c2a62c8851b047337689ee1a64b11cf
ffanizza <ffanizza@hawk.iit.edu>
2017-11-11 23:56:55 -0600

    added substitutions (with alternatives) and ratios

--- background.js
+++ background.js
@@ -24,25 +24,102 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 var replaceWords = function () {
 	
 		var elements = document.getElementsByTagName('*');
-	
-		// // var keyString = 'a string',
-		// //     keyObj = {},
-		// //     keyFunc = function() {};
-		//
-		// setting the values
-		var veganMap = {
-			"egg": "tofu",
-			"milk": "almondmi1k"
+
+		var veganMap  = {
+			"milk" : [
+				{"soymilk": "1"},
+				{"rice milk": "1"},
+				{"oat milk": "1"},
+				{"hemp milk": "1"},
+				{"nut milk": "1"}
+			],
+			"ricotta" : [
+				{"crumbled tofu" : "1"},
+				{"soaked raw nuts" : "1"}
+			],
+			"mozzarella" : [
+				{"daiya mozzarella" : "1"}
+			],
+			"cream cheese" : [
+				{"daiya cream cheese" : "1"}
+			],
+			"cheese" : [
+				{"vegan cheese" : "1"}
+			],
+			"eggs (baking)" : [
+				{"applesauce" : "1"},
+				{"pureed soft tofu" : "1"},
+				{"Ener-G" : "1"},
+				{"1 tablespoon ground flax seeds plus 3 tablespoons water or other liquid, blended" : "1"},
+				{"mashed bananas" : "1"}
+			],
+			"eggs (binding)" : [
+				{"oat flour" : "1"},
+				{"bread crumbs" : "1"},
+				{"instant potatoes" : "1"},
+				{"tomato paste" : "1"}
+			],
+			"eggs white" : [
+				{"aguafaba" : "1"}
+			],
+			"beef stock" : [
+				{"vegetable stock" : "1"}
+			],
+			"chicken stock" : [
+				{"vegetable stock" : "1"}
+			],
+			"beef" : [
+				{"tofu" : "1"}
+			],
+			"butter" : [
+				{"vegan butter" : "1"},
+				{"coconut oil" : "1"}
+			],
+			"yogurt" : [
+				{"vegan yogurt" : "1"}
+			],
+			"sour cream" : [
+				{"vegan yogurt" : "1"},
+				{"blended silken yogurt" : "1"}
+			],
+			"mayonnaise" : [
+				{"vegan mayonnaise" : "1"}
+			],
+			"gelatin" : [
+				{"agar" : "1"}
+			],
+			"honey" : [
+				{"maple syrup" : "1"},
+				{"agave nectar" : "1"},
+				{"molasses" : "1"}
+			],
+			"chocolate" : [
+				{"vegan chocolate" : "1"}
+			],
+			"ice cream" : [
+				{"vegan ice cream" : "1"}
+			],
+			"cream" : [
+				{"full-fat coconut milk" : "1"}
+			],
+			"beef" : [
+				{"eggplant" : "1"},
+				{"mushroom" : "1"}
+			],
+			"pork" : [
+				{"jackfruit" : "1"}
+			],
+			"chicken" : [
+				{"tempeh" : "1"}
+			],
+			"lamb" : [
+				{"seitan" : "1"}
+			],
+			"fish stock" : [
+				{"steeped kombu" : "1"}
+			]
 		}
-		// // getting the values
-		// myMap.get(keyString);    // "value associated with 'a string'"
-		// myMap.get(keyObj);       // "value associated with keyObj"
-		// myMap.get(keyFunc);      // "value associated with keyFunc"
-		//
-		// myMap.get('a string');   // "value associated with 'a string'"
-		//                          // because keyString === 'a string'
-		// myMap.get({});           // undefined, because keyObj !== {}
-		// myMap.get(function() {}) // undefined, because ke
+		
 	
 		for (var element of elements) {
 			for (var node of element.childNodes) {
