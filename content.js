19299ad6629dd034e3b22c775c48b438f7890bd4
kmui2 <kmui2@wisc.edu>
2017-11-12 12:05:37 -0600

    reverted to previous working version

--- content.js
+++ content.js
@@ -17,6 +17,107 @@ $.ajax({
             veganMap[mapping[nonVegan].title] = mapping[nonVegan].post;
         }
         console.log(veganMap);
+        // setting the values
+        // veganMap  = {
+        //     "milk" : [
+        //         "soymilk",
+        //         "rice milk",
+        //         "oat milk",
+        //         "hemp milk",
+        //         "nut milk"
+        //     ],
+        //     "ricotta" : [
+        //         "crumbled tofu" ,
+        //         "soaked raw nuts" 
+        //     ],
+        //     "mozzarella" : [
+        //         "daiya mozzarella" 
+        //     ],
+        //     "cream cheese" : [
+        //         "daiya cream cheese" 
+        //     ],
+        //     "cheese" : [
+        //         "vegan cheese" 
+        //     ],
+        //     "egg yolk" : [
+        //         "olive oil" 
+        //     ],
+        //     "egg white" : [
+        //         "aguafaba" 
+        //     ],
+        //     "egg" : [
+        //         "applesauce" ,
+        //         "pureed soft tofu" ,
+        //         "Ener-G" ,
+        //         "1 tablespoon ground flax seeds plus 3 tablespoons water or other liquid, blended" ,
+        //         "mashed bananas" 
+        //     ],
+        //     "eggs (binding)" : [
+        //         "oat flour" ,
+        //         "bread crumbs" ,
+        //         "instant potatoes" ,
+        //         "tomato paste" 
+        //     ],
+        //     "beef stock" : [
+        //         "vegetable stock" 
+        //     ],
+        //     "chicken stock" : [
+        //         "vegetable stock" 
+        //     ],
+        //     "beef" : [
+        //         "tofu" 
+        //     ],
+        //     "butter" : [
+        //         "vegan butter" ,
+        //         "coconut oil" 
+        //     ],
+        //     "yogurt" : [
+        //         "vegan yogurt" 
+        //     ],
+        //     "sour cream" : [
+        //         "vegan yogurt" ,
+        //         "blended silken yogurt" 
+        //     ],
+        //     "mayonnaise" : [
+        //         "vegan mayonnaise" 
+        //     ],
+        //     "gelatin" : [
+        //         "agar" 
+        //     ],
+        //     "honey" : [
+        //         "maple syrup" ,
+        //         "agave nectar" ,
+        //         "molasses" 
+        //     ],
+        //     "chocolate" : [
+        //         "vegan chocolate" 
+        //     ],
+        //     "ice cream" : [
+        //         "vegan ice cream" 
+        //     ],
+        //     "cream" : [
+        //         "full-fat coconut milk" 
+        //     ],
+        //     "beef" : [
+        //         "eggplant" ,
+        //         "mushroom" 
+        //     ],
+        //     "turkey" : [
+        //         "tofurkey" 
+        //     ],
+        //     "pork" : [
+        //         "jackfruit" 
+        //     ],
+        //     "chicken" : [
+        //         "tempeh" 
+        //     ],
+        //     "lamb" : [
+        //         "seitan" 
+        //     ],
+        //     "fish stock" : [
+        //         "steeped kombu" 
+        //     ]
+        // }
         
         chrome.runtime.onMessage.addListener(
             function (request, sender, sendResponse) {
@@ -35,92 +136,42 @@ $.ajax({
             }
         );
         
-        // function convertToVegan() {
-        
-        //     var elements = document.getElementsByTagName('*');
-        //     for (var nonVegan in veganMap) {
-        //         for (var element of elements) {
-        //             for (var node of element.childNodes) {
-        //                 // get text part of the element
-        //                 if (node.nodeType === 3) {
-        //                     var text = node.nodeValue;
-        //                     var newRegex;
-        //                     if (nonVegan.includes(" ")) {
-        //                         var nonVeganSplitList = nonVegan.split(" ");
-        //                         var regexBuilder = '';
-        //                         var isFirst = true;
-        //                         for (var word of nonVeganSplitList) {
-        //                             if (isFirst) {
-        //                                 regexBuilder += word;
-        //                                 isFirst = false;
-        //                             } else {
-        //                                 regexBuilder += ' ' + word;
-        //                             }
-        //                             regexBuilder += 's?';
-        //                         }
-        //                         newRegex = new RegExp(regexBuilder, "gi");
-        //                     } else {
-        //                         newRegex = new RegExp('[\\w-]*' + nonVegan + 's?[\\w-]*', "gi");
-        //                     }
-        //                     var textToInject = veganMap[nonVegan][0];
-        //                     // var textToInject = injectedSelect(veganMap[nonVegan]);
-        //                     var replacedText = text.replace(newRegex, textToInject);
-        
-        //                     if (replacedText !== text) {
-        //                         element.replaceChild(document.createTextNode(replacedText), node);
-        //                     }
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
-
         function convertToVegan() {
-            
-               var elements = document.getElementsByTagName('*');
-                for (var nonVegan in veganMap) {
-                    for (var element of elements) {
-                        for (var node of element.childNodes) {
-                            // get text part of the element
-                            if (node.nodeType === 3) {
-                                var text = node.nodeValue;
-                                var newRegex;
-                                if (nonVegan.includes(" ")) {
-                                    var nonVeganSplitList = nonVegan.split(" ");
-                                    var regexBuilder = '';
-                                    var isFirst = true;
-                                    for (var word of nonVeganSplitList) {
-                                        if (isFirst) {
-                                            regexBuilder += word;
-                                            isFirst = false;
-                                        } else {
-                                            regexBuilder += ' ' + word;
-                                        }
-                                        regexBuilder += 's?';
+        
+            var elements = document.getElementsByTagName('*');
+            for (var nonVegan in veganMap) {
+                for (var element of elements) {
+                    for (var node of element.childNodes) {
+                        // get text part of the element
+                        if (node.nodeType === 3) {
+                            var text = node.nodeValue;
+                            var newRegex;
+                            if (nonVegan.includes(" ")) {
+                                var nonVeganSplitList = nonVegan.split(" ");
+                                var regexBuilder = '';
+                                var isFirst = true;
+                                for (var word of nonVeganSplitList) {
+                                    if (isFirst) {
+                                        regexBuilder += word;
+                                        isFirst = false;
+                                    } else {
+                                        regexBuilder += ' ' + word;
                                     }
-                                    newRegex = new RegExp(regexBuilder, "gi");
-                                } else {
-                                    newRegex = new RegExp('[\\w-]*' + nonVegan + 's?[\\w-]*', "gi");
+                                    regexBuilder += 's?';
                                 }
-                                // var textToInject = veganMap[nonVegan][0];
-                                var textToInject = injectSelect(veganMap[nonVegan]);
-                                var replacedText = text.replace(newRegex, textToInject);
-                                if (replacedText !== text) {
-                                    element.replaceChild(document.createTextNode(replacedText), node);
-                                }
+                                newRegex = new RegExp(regexBuilder, "gi");
+                            } else {
+                                newRegex = new RegExp('[\\w-]*' + nonVegan + 's?[\\w-]*', "gi");
+                            }
+                            var replacedText = text.replace(newRegex, veganMap[nonVegan][0]);
+        
+                            if (replacedText !== text) {
+                                element.replaceChild(document.createTextNode(replacedText), node);
                             }
                         }
                     }
                 }
             }
-
-        function injectSelect(JSON) {
-            var select = document.createElement("SELECT");
-             for (var i = 0; i < JSON.length; i++ ) {
-                var currOption = document.createTextNode(JSON[i]);
-                select.appendChild(currOption);
-            }
-            return select;
         }
         
         function convertBack() {
